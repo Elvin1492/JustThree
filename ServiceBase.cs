9e10ee0f0045e04704573766344083939060686d
emammadov <Elvin.Mammadov@softeq.com>
2022-12-02 15:17:47 +0400

    ddd

--- ServiceBase.cs
+++ ServiceBase.cs
@@ -2,7 +2,7 @@ using StartUpApp.Core;
 using StartUpApp.Repository;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
